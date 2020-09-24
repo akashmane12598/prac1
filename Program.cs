b62c113be506336913d8c65d317c9defaa8c1a28
Akash <akashmane12598@gmail.com>
2020-09-24 18:36:29 +0530

    Hitman

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            int b = 5;
+            Console.WriteLine("Hello World "+9*5);
+        }
+    }
+}
